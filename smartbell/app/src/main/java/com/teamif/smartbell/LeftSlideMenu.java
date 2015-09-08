bbbb474035ffa92b5169bbef1ce2469fc1a5af0d
hgk333 <kys4543@gmail.com>
2015-09-08 12:56:29 +0900

    0908
    alram, edit the information of professor

--- smartbell/app/src/main/java/com/teamif/smartbell/LeftSlideMenu.java
+++ smartbell/app/src/main/java/com/teamif/smartbell/LeftSlideMenu.java
@@ -2,13 +2,19 @@ package com.teamif.smartbell;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.media.AudioManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Switch;
@@ -22,6 +28,9 @@ public class LeftSlideMenu extends Activity implements View.OnClickListener, Com
     TextView tvProfessor;
     TextView tvFeedback;
     TextView tvSetting;
+    TextView tvSettingName;
+    TextView tvSettingAddress;
+    TextView tvSettingEmail;
     LinearLayout LLPro;
     LinearLayout LLBase;
     LinearLayout LLFeed;
@@ -32,7 +41,12 @@ public class LeftSlideMenu extends Activity implements View.OnClickListener, Com
     boolean stateOfAram;
     Switch swAram;
     Switch swEti;
+    Button btEditInfoOfPro;
 
+    String pfName;
+    String pfAddress;
+    String pfEmail;
+    AudioManager checkAudio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +59,9 @@ public class LeftSlideMenu extends Activity implements View.OnClickListener, Com
         tvProfessor = (TextView)findViewById(R.id.tvProfessor); // 페이지이동
         tvFeedback = (TextView)findViewById(R.id.tvFeedback); //페이지이동
         tvSetting = (TextView)findViewById(R.id.tvSetting); //페이지이동
+        tvSettingName = (TextView)findViewById(R.id.tvSettingName);
+        tvSettingAddress = (TextView)findViewById(R.id.tvSettingAddress);
+        tvSettingEmail = (TextView)findViewById(R.id.tvSettingEmail);
         LLPro = (LinearLayout)findViewById(R.id.LLPro);
         LLBase = (LinearLayout)findViewById(R.id.LLbase);
         ivBackPro = (ImageView)findViewById(R.id.ivBackPro);
@@ -54,6 +71,7 @@ public class LeftSlideMenu extends Activity implements View.OnClickListener, Com
         LLSetting = (LinearLayout)findViewById(R.id.LLSetting);
         swAram = (Switch)findViewById(R.id.swAram);
         swEti = (Switch)findViewById(R.id.swEti);
+        btEditInfoOfPro = (Button)findViewById(R.id.btEditInfoOfPro);
 
         tvProfessor.setOnClickListener(this);
         tvFeedback.setOnClickListener(this);
@@ -63,7 +81,20 @@ public class LeftSlideMenu extends Activity implements View.OnClickListener, Com
         ivBackPro3.setOnClickListener(this);
         swAram.setOnCheckedChangeListener(this);
         swEti.setOnCheckedChangeListener(this);
+        btEditInfoOfPro.setOnClickListener(this);
 
+        checkAudio = (AudioManager) this.getSystemService(this.AUDIO_SERVICE);
+        switch (checkAudio.getRingerMode()) {
+            case AudioManager.RINGER_MODE_VIBRATE:
+            case AudioManager.RINGER_MODE_SILENT:
+                //진동 혹은 무음
+                swEti.setChecked(true);
+                break;
+            case AudioManager.RINGER_MODE_NORMAL:
+                //소리
+                swEti.setChecked(false);
+                break;
+        }
         viewPage();
         LLBase.setVisibility(View.VISIBLE);
         //이까지함 연결중....!
@@ -121,6 +152,70 @@ public class LeftSlideMenu extends Activity implements View.OnClickListener, Com
                 viewPage();
                 LLBase.setVisibility(View.VISIBLE);
                 break;
+            case R.id.btEditInfoOfPro :
+                AlertDialog.Builder dialog1 = new AlertDialog.Builder(this);
+                dialog1.setTitle("교수님 성함");
+                dialog1.setMessage("이름을 입력하세요");
+                // Set an EditText view to get user input
+                final EditText etInput1 = new EditText(this);
+                dialog1.setView(etInput1);
+                AlertDialog.Builder builder = dialog1.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        pfName = etInput1.getText().toString();
+                        // Do something with value!
+                        tvName.setText(pfName);
+                        tvSettingName.setText(pfName);
+                        //add to code for send the information to server
+                        AlertDialog.Builder dialog2 = new AlertDialog.Builder(LeftSlideMenu.this);
+                        dialog2.setTitle("교수님 메일");
+                        dialog2.setMessage("메일주소를 입력하세요");
+                        // Set an EditText view to get user input
+                        final EditText etInput2 = new EditText(LeftSlideMenu.this);
+                        dialog2.setView(etInput2);
+                        dialog2.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                pfEmail = etInput2.getText().toString();
+                                tvSettingEmail.setText(pfEmail);
+                                //add to code for sending the information to server
+                                AlertDialog.Builder alert = new AlertDialog.Builder(LeftSlideMenu.this);
+                                alert.setTitle("교수님 연구실");
+                                alert.setMessage("연구실 주소를 입력하세요");
+                                // Set an EditText view to get user input
+                                final EditText etInput3 = new EditText(LeftSlideMenu.this);
+                                alert.setView(etInput3);
+                                alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int whichButton) {
+                                        pfAddress = etInput3.getText().toString();
+                                        tvSettingAddress.setText(pfAddress);
+                                        //add to code for sending the information to server
+                                    }
+                                });
+                                alert.setNegativeButton("Cancel",
+                                        new DialogInterface.OnClickListener() {
+                                            public void onClick(DialogInterface dialog, int whichButton) {
+                                                // Canceled.
+                                            }
+                                        });
+                                alert.show();
+                            }
+                        });
+                        dialog2.setNegativeButton("Cancel",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int whichButton) {
+                                        // Canceled.
+                                    }
+                                });
+                        dialog2.show();
+                    }
+                });
+                dialog1.setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                // Canceled.
+                            }
+                        });
+                dialog1.show();
+                break;
         }
     }
     private void viewPage(){
@@ -149,19 +244,16 @@ public class LeftSlideMenu extends Activity implements View.OnClickListener, Com
                 }
                 break;
             case R.id.swEti :
-                if(stateOfAram == false){
-                    swEti.setChecked(true);
-                    Toast.makeText(this,"알람이 off인 상태에선 바꿀수 없습니다",Toast.LENGTH_SHORT).show();
-                    break;
-                }
-                if(b == true) { // setting when eti switch is on
-                    Log.d("switch " ,"eti on");
+                if(b == false) { // setting when eti switch is on
+                    Log.d("switch ", "eti on");
                     Toast.makeText(this,"에티켓 On",Toast.LENGTH_SHORT).show();
+                    checkAudio.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
 
                 }
                 else{ // setting when eti switch is off
                     Log.d("switch " ,"eti of");
                     Toast.makeText(this,"에티켓 off", Toast.LENGTH_SHORT).show();
+                    checkAudio.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
                 }
                 break;
         }
